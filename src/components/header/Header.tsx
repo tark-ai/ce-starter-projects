86679683033ac96208844e2daa287ac41edc6575
Saransh <saransh@tarkai.com>
2026-02-07 18:12:36 +0530

    chore: removed unused components and files and upgraded to latest version

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,8 @@
-import StatusBar from "./StatusBar";
 import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <header className="w-full sticky top-0 z-50">
-      {/* <StatusBar /> */}
       <Navigation />
     </header>
   );
