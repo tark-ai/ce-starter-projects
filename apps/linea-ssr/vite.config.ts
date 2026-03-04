e886c572755a314cfb2d217698d57b0b9016995c
Saransh <saransh@tarkai.com>
2026-03-04 19:42:38 +0530

    feat: added vercel specific adapters to vite config to use bun runtime

--- apps/linea-ssr/vite.config.ts
+++ apps/linea-ssr/vite.config.ts
@@ -1,9 +1,21 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: { port: 8081 },
-  plugins: [tsconfigPaths(), tanstackStart(), react()],
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart(),
+    nitro({
+      vercel: {
+        functions: {
+          runtime: "bun1.x",
+        },
+      },
+    }),
+    react(),
+  ],
 });
